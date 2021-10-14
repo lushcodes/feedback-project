22c033d3d5b29928590794a6e0c34c5b20770c0f
lushcodes <lushcodes@gmail.com>
2021-10-14 21:05:57 +0100

    learned to import components to app using svelte

--- svelte-feeback-app/src/App.svelte
+++ svelte-feeback-app/src/App.svelte
@@ -1,75 +1,28 @@
 <script>
-let firstName = 'Luke';
-let lastName = 'Usher';
-let color = 'blue';
-let showText = false;
-let users = [
-	{
-		id: '1',
-		name: 'John'
-	},
+	import FeedbackList from './components/FeedbackList.svelte'
 	
-	{
-		id: '2',
-		name: 'James'
-	},
-	
-	{
-		id: '3',
-		name: 'Jimmy'
-	}
-	,
-	
-	{
-		id: '4',
-		name: 'Jeff'
-	}
-]
+let feedback = [  {
+    id: 1,
+    rating: 10,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+  },]
 
-
-
-$: name = firstName + ' ' + lastName
-
-const toggle = () => {
-color = color === 'blue' ? 'red' : 'blue'
-showText = !showText
-users = [...users, {id: '5', name: 'Jen'}]
-}
 </script>
 
 <main>
-	<h1 style="color: {color}">Hello {name}!</h1>
-	{#if showText}
-		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-
-	{:else}
-		<p>NO TEXT</p>
-	{/if}
-	<button on:click={toggle}>Click</button>
-
-	{#each users as user (user.id)}
-		<h3>{user.id}: {user.name}</h3>
-	{/each}
+	<FeedbackList />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
